3054efe959df709f30ca77ab8c26d00c6f874b40
Ethan_Johnson <ej_johns04@outlook.com>
2022-02-08 17:43:00 -0600

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -9,7 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location=search.value
-    
+
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
