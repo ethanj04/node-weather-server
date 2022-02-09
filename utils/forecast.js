1bde2119f2875af337d2603c926587de454e41e2
Ethan_Johnson <ej_johns04@outlook.com>
2022-02-08 20:16:27 -0600

    Adding in mph and wind direction

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,10 +9,9 @@ request ( {url, json:true}, (error, {body}) => {
     } else if( body.error ) {
         callback("Unable to find location!", undefined) 
     } else {
-        callback( undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees outside. It feels like " +  body.current.feelslike  +  " outside. ")
+        callback( undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees outside. It feels like " +  body.current.feelslike  +  " outside. \n\n  The wind speed is " + body.current.wind_speed+ " mph and the wind direction is " + body.current.wind_dir +".")
     }
 })
 }
 
 module.exports = forecast
-
